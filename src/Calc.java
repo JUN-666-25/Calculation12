72568f9ddbebe24574bbb8bc4ad1e7445dd8768f
JUN-666-25 <junya.6660924@gmail.com>
2025-06-13 16:23:01 +0900

    printAdd()とprintSub()の引数を変更

--- src/Calc.java
+++ src/Calc.java
@@ -3,7 +3,24 @@ public class Calc {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+		printAdd(2,2);
+		
+		printSub(5,1);
 
 	}
+		public static void printAdd(int arg1,int arg2) {
+			int sum =arg1+arg2;
+			String result=arg1+"+"+arg2+"="+sum;
+			System.out.println(result);
+		
+	}
+		
+		public static void printSub(int arg1,int arg2) {
+			int sub = arg1-arg2;
+			String result2 = arg1 + "-" + arg2 + "=" + sub;
+			System.out.println(result2);
+			
+		}
 
 }
